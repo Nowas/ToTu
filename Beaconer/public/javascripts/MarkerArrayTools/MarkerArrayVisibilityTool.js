a74d87e68ff5b1b9eaf8e2178d6dfebaa8fcac89
Patryk Nowacki <nowas5@gmail.com>
2016-03-13 22:54:39 +0100

    Marker visibility tool

--- Beaconer/public/javascripts/MarkerArrayTools/MarkerArrayVisibilityTool.js
+++ Beaconer/public/javascripts/MarkerArrayTools/MarkerArrayVisibilityTool.js
@@ -1,63 +1,20 @@
-var  MarkerArrayAvailabilityTool = function(){
-        var prevMarkers = [];
-        var removedMarkers = [];
-        var addedMarkers = [];
-        var modifiedMarkers = [];
-        
-        function newData(newVehicles){
-            newVehicles = newVehicles.sort(sortVehicleFn);
-            removedMarkers.splice(0,removedMarkers.length);
-            modifiedMarkers.splice(0,modifiedMarkers.length);
-            addedMarkers.splice(0,addedMarkers.length);
+var  MarkerArrayVisibilityTool = function(){
 
-            var prevIt=0, newIt=0;
-
-            while( prevIt < prevMarkers.length || newIt < newVehicles.length )
-            {
-                if (prevMarkers[prevIt] < newVehicles[newIt] ||
-                        newIt == newVehicles.length){
-                    removedMarkers.push(prevMarkers[prevIt]);
-                    prevIt++;
-                }
-                else if (prevMarkers[prevIt] > newVehicles[newIt] ||
-                        prevIt == prevMarkers.length){ 
-                    addedMarkers.push(newVehicles[newIt]);
-                    newIt++;
-                }
+        function run(selecteDisplayText, markers){
+            markers.forEach(function(entry){
+                if( selecteDisplayText == null || entry.displayText == selecteDisplayText)
+                    entry.visible = true;
                 else
-                {
-                    modifiedMarkers.push(prevMarkers[prevIt]);
-                    prevIt++;
-                    newIt++;
-                }
-            }
-            prevMarkers = newVehicles;
+                    entry.visible = false;
+            });
+            return markers;
         }
         
-        function sortVehicleFn(a, b){
-            return a.ID.localeCompare(b.ID);
-        }
-        
-        function getRemovedMarkers(){
-            return removedMarkers;
-        }
-
-        function getAddedMarkers(){
-            return addedMarkers;
-        }
-
-        function getModifiedMarkers(){
-            return modifiedMarkers;
-        }
-
         return {
-            newData:newData,
-            getRemovedMarkers:getRemovedMarkers,
-            getModifiedMarkers:getModifiedMarkers,
-            getAddedMarkers:getAddedMarkers
+            run:run,
         }
     };
 
 if (typeof module !== "undefined" && module.exports) {
-    module.exports = MarkerArrayAvailabilityTool;
+    module.exports = MarkerArrayVisibilityTool;
 }
