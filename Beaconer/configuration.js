dafc5aad7324b17fe0f36b13782732ccde59093d
Patryk Nowacki <nowas5@gmail.com>
2016-03-18 22:17:42 +0100

    Multiple beacons data for beaconer

---
+++ Beaconer/configuration.js
@@ -0,0 +1,5 @@
+
+exports.getConfig = function(){
+    return [{name:'vehBeacon1', url:'http://localhost:3000/vehicles'},
+    {name:'vehBeacon2', url:'http://localhost:3001/vehicles'}];
+};
--- Beaconer/routes/data.js
+++ Beaconer/routes/data.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var request = require('request');
+var beaconerConf = require('../configuration');
 var router = express.Router();
-var config = [{name:'vehBeacon1', url:'http://localhost:3000/vehicles'}];
 
 /* GET home page. */
 router.get('/vehicles', function(req, res, next) {
-    request(config[0].url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.jsonp(JSON.parse(body).data);
-        }
-    })
+    var responses = [];
+    var completed_request = 0;
+    var beacons = beaconerConf.getConfig();
+    beacons.forEach(function(confEntry){
+        request(confEntry.url, function (error, response, body) {
+            completed_request++;
+            if (!error && response.statusCode == 200) {
+                responses.push(JSON.parse(body).data);
+                if (completed_request == beacons.length) {
+                    var merged = [].concat.apply([], responses)
+                    res.jsonp(merged);
+                }
+            }
+        })
+    });
 });
 
 module.exports = router;
