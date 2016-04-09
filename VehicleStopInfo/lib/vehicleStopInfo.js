62abcced41c088502f3c77db44cbb35da07831b3
Patryk Nowacki <nowas5@gmail.com>
2016-04-10 00:22:32 +0200

    VehicleStopInfoService

--- VehicleStopInfo/lib/vehicleStopInfo.js
+++ VehicleStopInfo/lib/vehicleStopInfo.js
@@ -94,49 +94,32 @@ exports.getVehicleStopInfo = function(id,callback) {
     pool.open(cn, function (err, db) {
         if (err) 
             return console.log('e1' + err);
-        var date = new Date();
-        var hour = date.getHours();
-        var min  = date.getMinutes();
-        var sec  = date.getSeconds();
-        var totalMinutes = hour * 60 + min;
-        var totalSec = totalMinutes * 60 + sec;
 
-        db.query('SELECT TLI_NAZWA as LINE,' +
-        ' MIN24,TLI_OST_PST_NAZWA AS DEST,' +
-        ' PST_NAZWA as STOP_NAME,' +
-        ' GODZ_ODJ as DEP_H, MIN_ODJ as DEP_M' +
+        db.query('SELECT TLI_NAZWA as line,' +
+        ' TLI_OST_PST_NAZWA AS direction,' +
+        ' PST_NAZWA as stopName,' +
+        ' GODZ_ODJ as depH, MIN_ODJ as depM' +
         ' FROM VIEW_ODJAZDY' +
-        ' WHERE PST_ID = ?' + 
-        ' ORDER BY MIN24', [id.toString(), totalMinutes.toString()], function (err, data) {
+        ' WHERE PST_ID = ?', [id.toString()], function (err, data) {
             if (err)
                 console.log('e2' + err);
-            if(data.length == 0)
-                return callback([]); 
-            var routes = {};
-            data.forEach(function(entry){
-                if(!routes[entry.LINE])
-                {
-                    routes[entry.LINE] = {};
-                    routes[entry.LINE]['departures'] = {}
-                }
-                if(!routes[entry.LINE]['departures'][entry.DEP_H])
-                    routes[entry.LINE]['departures'][entry.DEP_H] = [];
-                    
-                var nextDep = ((entry.MIN24 * 60 - totalSec) + 24*60*60) % (24*60*60);
-                var nextDepText = "";
-                if (nextDep > 3600)
-                    nextDepText = entry.DEP_H%24 + ":" + entry.DEP_M; 
-                else if (nextDep > 60)
-                    nextDepText = Math.floor(nextDep / 60) + " min";
-                else
-                    nextDepText = nextDep + " sek";
-                if( !routes[entry.LINE]['nextDep'] && entry.MIN24 > totalMinutes)
-                    routes[entry.LINE]['nextDep'] = nextDepText;
-                routes[entry.LINE]['departures'][entry.DEP_H].push( entry.DEP_M);
-            });
-
             db.close();
-            callback(routes);
+            if(data.length == 0)
+                return callback([]);
+            data = data.map(function(e){
+                return  {
+                    line:e.LINE,
+                    direction:e.DIRECTION,
+                    stopName:e.STOPNAME,
+                    depH:e.DEPH,
+                    depM:e.DEPM
+                };
+                
+            });
+            callback({
+                stopName: data[0].stopName,
+                timeTable: exports.getTimeTable(data)
+            });
         });
     });
     
